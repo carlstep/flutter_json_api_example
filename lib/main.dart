4d5867be9e673877b24169b2118158178eef5720
carlstep <carlstep333@me.com>
2023-01-26 14:42:14 +0700

    returns 1 item from Album json data

--- lib/main.dart
+++ lib/main.dart
@@ -75,15 +75,7 @@ class _MyAppState extends State<MyApp> {
               future: futureAlbum,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ListTile(
-                        leading: Text(snapshot.data!.userId.toString()),
-                        title: Text(snapshot.data!.title),
-                      )
-                    ],
-                  );
+                  return Text(snapshot.data!.title);
                 } else if (snapshot.hasError) {
                   return Text('${snapshot.error}');
                 }
